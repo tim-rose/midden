4b3c577f2be17928210301fe6d3e6d4b54ec446b
Tim Rose <tim.rose@acm.org>
2021-08-29 16:57:22 +1000

    fixes echo(1) flag interference

--- test/test-getopt.sh
+++ test/test-getopt.sh
@@ -7,65 +7,72 @@ PATH=..:$PATH
 require getopt
 require tap
 
-plan 30
+plan 33
 
 #
 # getopt_short_spec() tests
 #
 ok_eq "$(getopt_short_spec 'a.alpha;b.beta;g.gamma')" 'abg' \
-    'getopt_short_spec: flags only'
+      'getopt_short_spec: flags only'
 
 ok_eq "$(getopt_short_spec 'a.alpha;b.beta=;g.gamma')" 'ab:g' \
-    'getopt_short_spec: option with value; but no default'
+      'getopt_short_spec: option with value; but no default'
 
 ok_eq "$(getopt_short_spec 'a.alpha;b.beta=value;g.gamma')" 'ab:g' \
-    'getopt_short_spec: option with simple default value'
+      'getopt_short_spec: option with simple default value'
 
 ok_eq "$(getopt_short_spec 'a.alpha;b.beta=value.txt;g.gamma')" 'ab:g' \
-    'getopt_short_spec: default values can contain "."'
+      'getopt_short_spec: default values can contain "."'
 
 ok_eq "$(getopt_short_spec 'a.alpha;b.beta=value txt;g.gamma')" 'ab:g' \
-    'getopt_short_spec: default values can contain " "'
+      'getopt_short_spec: default values can contain " "'
+
+ok_eq "$(getopt_short_spec 'n.number;e.error')" 'ne' \
+      'getopt_short_spec: not confused by echo(1) options'
+
 #
 # getopt_long_spec() tests
 #
 ok_eq "$(getopt_long_spec 'a.alpha;b.beta;g.gamma')" 'alpha,beta,gamma' \
-    'getopt_long_spec: flags only'
+      'getopt_long_spec: flags only'
 
 ok_eq "$(getopt_long_spec 'a.alpha;b.beta=;g.gamma')" 'alpha,beta:,gamma' \
-    'getopt_long_spec: option with value; but no default'
+      'getopt_long_spec: option with value; but no default'
 
 ok_eq "$(getopt_long_spec 'a.alpha;b.beta=value;g.gamma')" 'alpha,beta:,gamma' \
-    'getopt_long_spec: option with simple default value'
+      'getopt_long_spec: option with simple default value'
 
 ok_eq "$(getopt_long_spec 'a.alpha;b.beta=value.txt;g.gamma')" 'alpha,beta:,gamma' \
-    'getopt_long_spec: default values can contain "."'
+      'getopt_long_spec: default values can contain "."'
 
 ok_eq "$(getopt_long_spec 'a.alpha;b.beta=value txt;g.gamma')" 'alpha,beta:,gamma' \
-    'getopt_long_spec: default values can contain " "'
+      'getopt_long_spec: default values can contain " "'
 
 ok_eq "$(getopt_long_spec 'a.alpha;b.be_ta=value txt;g.gamma')" 'alpha,be-ta:,gamma' \
-    'getopt_long_spec: variable names have "_" mapped to "-"'
+      'getopt_long_spec: variable names have "_" mapped to "-"'
+
+ok_eq "$(getopt_long_spec 'n.number;e.error')" 'number,error' \
+      'getopt_long_spec: not confused by echo(1) options'
 
 #
 # getopt_defaults() tests.
 #
 ok_eq "$(getopt_defaults 'a.alpha;b.beta;g.gamma')" '' \
-    'getopt_defaults: flags only'
+      'getopt_defaults: flags only'
 
 ok_eq "$(getopt_defaults 'a.alpha;b.beta=;g.gamma')" 'beta="";' \
-    'getopt_defaults: empty value'
+      'getopt_defaults: empty value'
 
 ok_eq "$(getopt_defaults 'a.alpha;b.beta=value;g.gamma')" 'beta="value";' \
-    'getopt_defaults: simple value'
+      'getopt_defaults: simple value'
 
 ok_eq "$(getopt_defaults 'a.alpha;b.beta=value.txt;g.gamma')" \
-    'beta="value.txt";' \
-    'getopt_defaults: value containing "."'
+      'beta="value.txt";' \
+      'getopt_defaults: value containing "."'
 
 ok_eq "$(getopt_defaults 'a.alpha;b.beta=value txt;g.gamma')" \
-    'beta="value txt";' \
-    'getopt_defaults: value containing " "'
+      'beta="value txt";' \
+      'getopt_defaults: value containing " "'
 
 #
 # getopt_short_var() tests.
@@ -88,30 +95,30 @@ opts='a.alpha;b.beta=value;g.gamma'
 
 OPTIND=1			# reset getopt state!
 ok_grep "$(getopt_args "$opts" -x 2>/dev/null)" "exit 2;" \
-    'getopt_args: invalid flag causes exit'
+	'getopt_args: invalid flag causes exit'
 
 OPTIND=1
 ok_eq "$(getopt_args "$opts")" 'OPTIND=1' 'getopt_args: empty arglist'
 
 OPTIND=1
 ok_eq "$(getopt_args "$opts" -a)" \
-    "$(printf '%s\n' 'alpha=1;' 'OPTIND=2')" \
-    'getopt_args: single flag specified'
+      "$(printf '%s\n' 'alpha=1;' 'OPTIND=2')" \
+      'getopt_args: single flag specified'
 
 OPTIND=1
 ok_eq "$(getopt_args "$opts" -ag)" \
-    "$(printf '%s\n' 'alpha=1;' 'gamma=1;' 'OPTIND=2')" \
-    'getopt_args: two flags specified together'
+      "$(printf '%s\n' 'alpha=1;' 'gamma=1;' 'OPTIND=2')" \
+      'getopt_args: two flags specified together'
 
 OPTIND=1
 ok_eq "$(getopt_args "$opts" -a -g)" \
-    "$(printf '%s\n' 'alpha=1;' 'gamma=1;' 'OPTIND=3')" \
-    'getopt_args: two flags specified separately'
+      "$(printf '%s\n' 'alpha=1;' 'gamma=1;' 'OPTIND=3')" \
+      'getopt_args: two flags specified separately'
 
 OPTIND=1
 result=$(printf '%s\n' 'alpha=1;' 'gamma=1;' 'beta="foobar";' 'OPTIND=3')
 ok_eq "$(getopt_args "$opts" -agb foobar)" "$result" \
-    'getopt_args: flags and values specified'
+      'getopt_args: flags and values specified'
 
 #
 # getopt_args() "-d" tests
@@ -119,10 +126,16 @@ ok_eq "$(getopt_args "$opts" -agb foobar)" "$result" \
 OPTIND=1
 result=$(printf '%s\n' 'beta="value";' 'OPTIND=1')
 ok_eq "$(getopt_args -d "$opts")" "$result" \
-    'getopt_args: "-d" supplies default value'
+      'getopt_args: "-d" supplies default value'
 
 OPTIND=1
 result=$(printf '%s\n' 'beta="value";' 'beta="foobar";' 'OPTIND=3')
 ok_eq "$(getopt_args -d "$opts" -b foobar)" "$result" \
-    'getopt_args: "-d" command-line overrides defaults'
+      'getopt_args: "-d" command-line overrides defaults'
+
+OPTIND=1
+opts='n.number;e.error'
+result=$(printf '%s\n' 'number=1;' 'error=1;' 'OPTIND=2')
+ok_eq "$(getopt_args "$opts" -ne foobar)" "$result" \
+      'getopt_args: flags and values specified'
 exit 0
